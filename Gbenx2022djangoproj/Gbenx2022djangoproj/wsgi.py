270e26788d351e929ec064692d9d1421e4bd810d
awosika <awosikaolugbenga@gamil.com>
2022-06-04 14:00:04 +0100

    asisignment commit

---
+++ Gbenx2022djangoproj/Gbenx2022djangoproj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Gbenx2022djangoproj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gbenx2022djangoproj.settings')
+
+application = get_wsgi_application()
